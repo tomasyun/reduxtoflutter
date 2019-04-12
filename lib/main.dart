06dd990b03140f294c8336fc3b14f1d81ebbdadb
yunt <yuntuo19900228@126.com>
2019-04-12 16:56:09 +0800

    第一次提交

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:reduxtoflutter/app.dart';
+
+void main() => runApp(createApp());
---
+++ lib/todo_list_page/page.dart
@@ -0,0 +1,26 @@
+import 'package:fish_redux/fish_redux.dart';
+import 'package:reduxtoflutter/todo_list_page/report_component/component.dart';
+
+import 'effect.dart';
+import 'list_adapter/adapter.dart';
+import 'reducer.dart';
+import 'state.dart';
+import 'view.dart';
+
+class ToDoListPage extends Page<PageState, Map<String, dynamic>> {
+  ToDoListPage()
+      : super(
+          reducer: buildReducer(),
+          initState: initState,
+          effect: buildEffect(),
+          view: buildView,
+          dependencies: Dependencies<PageState>(
+              adapter: ToDoListAdapter(),
+              slots: <String, Dependent<PageState>>{
+                'report': ReportConnector() + ReportComponent()
+              }),
+          middleware: <Middleware<PageState>>[
+            logMiddleware(tag: 'ToDoListPage'),
+          ],
+        );
+}
